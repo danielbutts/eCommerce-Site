bb8032c27c3c5e2310277b1d8a79b995688213b0
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-03-09 14:04:45 -0800

    Fixed header footer and image links

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 $('form').on('submit', function(e) {
   e.preventDefault();
-  var valid = $('#email').val()
-  if (valid === '') {
-  $("#div1").fadeIn(500).delay(3000).fadeOut(500);
+  let userEmailInput = $('#email').val()
+
+  if (userEmailInput === '') {
+  $("#div1").fadeIn(300).delay(3000).fadeOut(300);
   } else {
-  $("#div2").fadeIn(500).delay(3000).fadeOut(500);
+  $("#div2").fadeIn(300).delay(3000).fadeOut(300);
   }
 });
